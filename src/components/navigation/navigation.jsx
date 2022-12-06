b20626f29a3c3f231f4494bfe93d810906520e93
Serhii Sukmaniuk <ssukmanyuk@gmail.com>
2022-12-06 20:55:56 +0200

    code optimization, added firebase observer for login logout

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,29 +1,41 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { signOutUser } from "../../utils/firebase/firebase-utils";
 
-import './navigation-styles.scss'
+import { UserContext } from "../contexts/user-context";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./navigation-styles.scss";
 
 const Navigation = () => {
-  return(
+  const { currentUser } = useContext(UserContext);
+
+  return (
     <Fragment>
-    <div className="navigation">
-      <Link className="logo-container" to="/">
-        <Logo className="logo" />
-      </Link>
-      <div className="nav-links-container">
-      <Link className="nav-link" to="/shop">
-        Shop
-      </Link>
-      <Link className="nav-link" to="/auth">
-        Sign in
-      </Link>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              Sign out
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              Sign in
+            </Link>
+          )}
+        </div>
       </div>
-    </div>
-    <Outlet />
-  </Fragment>
-  )
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
